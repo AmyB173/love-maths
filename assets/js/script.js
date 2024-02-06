0f3bdf6ec411aa9d60396e0fa27cd1ba471e25a8
AmyB173 <148871917+AmyB173@users.noreply.github.com>
2024-02-06 05:32:59 +0000

    add event listeners for buttons and display alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-function runGame(){}
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
+function runGame(){
+
+}
 
 function checkAnswer(){
 
@@ -14,14 +31,14 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion {
+function displayAdditionQuestion() {
 
 }
 
-function displaySubtractQuestion {
+function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion {
-    
+function displayMultiplyQuestion() {
+
 }
